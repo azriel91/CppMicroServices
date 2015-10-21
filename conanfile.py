ffc86089c07b2c866c82e56c0c1dddff45d1f53c
azriel <azriel91@gmail.com>
2015-10-22 10:40:43 +1300

    Added conanfile.py and updated .gitignores

---
+++ conanfile.py
@@ -0,0 +1,87 @@
+from conans import *
+import subprocess
+
+class CppMicroServicesConan(ConanFile):
+    name = 'CppMicroServices'
+    version = '2.99.0'
+    settings = ['os', 'compiler', 'build_type']
+    options = {
+        'US_ENABLE_AUTOLOADING_SUPPORT': 'OFF', # Enable bundle auto-loading support
+        'US_ENABLE_THREADING_SUPPORT':   'OFF', # Enable threading support
+        'US_ENABLE_DEBUG_OUTPUT':        'OFF', # Enable debug messages
+        'US_BUILD_SHARED_LIBS':          'ON',  # Build shared libraries
+        'US_BUILD_TESTING':              'OFF', # Build tests
+        'US_BUILD_EXAMPLES':             'OFF', # Build example projects
+    }
+    generators = ["cmake"]
+
+    # Prefer 'exports' over 'source' as we may want to build off a particular branch
+    # Can we assume running 'git ls-files' to be safe?
+    exports = subprocess.check_output(['git', 'ls-files']).split()
+
+    build_dir = 'build'
+    cppmicroservices_bundles = ['core','httpservice', 'shellservice', 'webconsole']
+
+    def build(self):
+        option_defines = ' '.join("-D%s=%s" % (option, val) for (option, val) in self.options.iteritems())
+        self.run("cmake . -B{build_dir} {defines}".format(build_dir=self.build_dir, defines=option_defines))
+        self.run("cmake --build {build_dir}".format(build_dir=self.build_dir))
+
+    def package(self):
+        # Module headers
+        for bundle in self.cppmicroservices_bundles:
+            include_path = "{bundle}/include".format(bundle=bundle)
+
+            # we copy headers from the src path as well because some of the public headers need these to compile
+            src_path = "{bundle}/src".format(bundle=bundle)
+            build_include_path = "{build_dir}/{bundle}/include".format(build_dir=self.build_dir, bundle=bundle)
+            self.copy('*.h', dst=include_path, src=include_path)
+            self.copy('*.h', dst=include_path, src=src_path)
+            self.copy('*.h', dst=include_path, src=build_include_path)
+
+        # Third party headers
+        self.copy('*.h', dst='third_party', src='third_party')
+
+        # Generated global includes
+        build_include_path = "{build_dir}/include".format(build_dir=self.build_dir)
+        self.copy('*.h', dst='include', src=build_include_path)
+
+        # Built artifacts
+        lib_dir = "{build_dir}/lib".format(build_dir=self.build_dir)
+        self.copy('*.so*', dst='lib', src=lib_dir) # In unix systems, the version number is appended
+        self.copy('*.a', dst='lib', src=lib_dir)
+        self.copy('*.lib', dst='lib', src=lib_dir)
+
+        # usWebConsole is built in the bin/main directory, need to check why
+        # In unix systems, the version number is appended to the shared library
+        self.copy('*.so*', dst='lib', src="{build_dir}/bin/main".format(build_dir=self.build_dir))
+        self.copy('*.dll', dst='lib', src="{build_dir}/bin".format(build_dir=self.build_dir))
+
+        # CMake info
+        self.copy('CppMicroServicesConfig.cmake', dst='.', src=self.build_dir)
+        self.copy('CppMicroServicesConfigVersion.cmake', dst='.', src=self.build_dir)
+        self.copy('CppMicroServicesTargets.cmake', dst='.', src=self.build_dir)
+
+        # CMake functions
+        # We need to copy '*' because there are also template code files in the cmake directory
+        self.copy('*', dst='cmake', src='cmake')
+
+    def package_info(self):
+        """ Maybe we shouldn't link to every bundle that is built, and just link to the CppMicroServices one """
+        self.cpp_info.libs = ['usHttpService', 'usShellService', 'CppMicroServices', 'usWebConsole']
+        self.cpp_info.includedirs += ["{bundle}/include".format(bundle=bundle)
+                                      for bundle in self.cppmicroservices_bundles]
+
+        # The self.copy function used in package recursively copies files under subdirectories, but includes the full
+        # path. For header includes to work, either the subdirectory paths must not be included when copying, or we add
+        # the subdirectories as directories on the compilation include paths
+        #
+        # Private headers under 'core/src/**' that were copied into 'core/include/**'
+        # Improvement: recursively include any directories underneath {bundle}/include
+        self.cpp_info.includedirs += ["core/include/{core_private}".format(core_private=sub_dir)
+                                      for sub_dir in ['bundle', 'service', 'util']]
+
+        # HACK: This is not the right way to get macros defined by other projects into cmake
+        self.cpp_info.includedirs += ['.']
+
+        self.output.info(vars(self.cpp_info))
